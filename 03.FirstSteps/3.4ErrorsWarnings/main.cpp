74bb0224173b6e687deea31d5be8e318e6baa541
Bharath <gub5bmh@bosch.com>
2025-11-28 13:38:51 +0530

    fix errors and added comments.

--- 03.FirstSteps/3.4ErrorsWarnings/main.cpp
+++ 03.FirstSteps/3.4ErrorsWarnings/main.cpp
@@ -2,11 +2,11 @@
 
 int main(){
    
-   //Compile time error
+   //Compile time error -> already fixed with ; in the base file
    std::cout << "Hello Word!" << std::endl;
 
-   //Run time error
-   int value = 7/0;
+   //Run time error -> fixed by changing 0 to 1 to avoid the divide by zero error but this was also caught during compilation.
+   int value = 7/1;
    std::cout << "value : " << value << std::endl;
 
     return 0;

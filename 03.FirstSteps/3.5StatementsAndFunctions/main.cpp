c08caa8dda5f9b6f187817822727ea6bb2e82252
Bharath <gub5bmh@bosch.com>
2025-11-28 14:07:01 +0530

    add function to sub and mul and call it in main.

--- 03.FirstSteps/3.5StatementsAndFunctions/main.cpp
+++ 03.FirstSteps/3.5StatementsAndFunctions/main.cpp
@@ -5,6 +5,12 @@ int addNumbers( int first_param, int second_param){
     return result;
 }
 
+int subnumbers(int first_param, int second_param);
+int mulnumbers(int first_param, int second_param)
+{
+    return first_param * second_param;
+}
+
 int main(){
 
     int first_number {13}; // Statement
@@ -23,7 +29,15 @@ int main(){
     std::cout << "Sum : " << sum << std::endl;
 
     std::cout << "Sum : " << addNumbers(3,42) << std::endl;
-
+    std::cout << "Diff : " << subnumbers(first_number, second_number) << std::endl;
+    std::cout << "mul : " << mulnumbers(first_number, second_number) << std::endl;
 
     return 0;
+}
+
+
+int subnumbers(int first_param, int second_param)
+{
+    int result = first_param - second_param;
+    return result;
 }

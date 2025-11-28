d42d94d0a4db5536f8f9a19f9b6166b8fd2db548
Bharath <gub5bmh@bosch.com>
2025-11-28 10:44:47 +0530

    add new file for dog bark function. Update cmakelists to reflect the changes

--- 03.FirstSteps/3.2FirstCppProgram/main.cpp
+++ 03.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include"dog.h"
 
 int main(){
 
     std::cout << "Number1" << std::endl;
     std::cout << "Number2" << std::endl;
     std::cout << "Number3" << std::endl;
-
+	bark();
     return 0;
 }
---
+++ 03.FirstSteps/3.2FirstCppProgram/src/dog.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include"dog.h"
+
+void bark(){
+	std::cout << "Woof Woof!" << std::endl;
+}
